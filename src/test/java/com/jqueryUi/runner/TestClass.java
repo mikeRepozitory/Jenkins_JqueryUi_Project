924496b804a925a359cf4c846ffa7f6aab5d408b
Mihai <mbqatester@gmail.com>
2021-07-02 19:25:49 -0400

    pom update for jenkis

---
+++ src/test/java/com/jqueryUi/runner/TestClass.java
@@ -0,0 +1,22 @@
+package com.jqueryUi.runner;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+
+    @Test
+    public void sampleTest_1() {
+        System.out.println("sampleTest_1");
+    }
+
+    @Test
+    public void sampleTest_2() {
+        System.out.println("sampleTest_2");
+    }
+
+    @Test
+    public void sampleTest_3() {
+        System.out.println("sampleTest_3");
+    }
+}
--- src/test/java/com/jqueryUi/step_definitions/Effects_Steps.java
+++ src/test/java/com/jqueryUi/step_definitions/Effects_Steps.java
@@ -8,5 +8,15 @@ import static com.jqueryUi.base.Hook.driver;
 
 public class Effects_Steps {
 
+    @Then("user selects AddClass button")
+    public void user_selects_add_class_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user click on Run Effect")
+    public void user_click_on_run_effect() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
