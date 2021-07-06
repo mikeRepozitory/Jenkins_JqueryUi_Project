783e3aaf311ca80ae0121ef4e5ba30d6d238ca3f
Mihai <mbqatester@gmail.com>
2021-07-05 20:26:51 -0400

    execution the etst cases 25% done

--- src/test/java/com/jqueryUi/runner/TestRunner.java
+++ src/test/java/com/jqueryUi/runner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
         features = {"src/test/resources/features"},
         glue = {"com/jqueryUi"},
         dryRun = false,
-        tags = ("@Interactions")
+        tags = ("@Draggable")
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
